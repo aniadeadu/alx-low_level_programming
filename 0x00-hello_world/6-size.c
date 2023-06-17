762e61444729bdae41fb75d94aed9caacf1e22ab
aniianii <anikeekanola@icloud.com>
2023-06-17 01:14:18 -0700

    Adding 5-printf.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * Return: 0
- */
+* main - main block
+* Return: 0
+*/
  
 int main(void)
 {
-	int v = 1, w = 4, x = 8, y = 8, z = 4;
-        printf("Size of a char: %i byte(s)\n", v);
-        printf("Size of an int: %i byte(s)\n", w);
-        printf("Size of a long int: %i byte(s)\n", x);
-        printf("Size of a long long int: %i byte(s)\n", y);
-        printf("Size of a float: %i byte(s)\n", z);
-        return (0);
+int v = 1, w = 4, x = 4, y = 8, z = 4;
+printf("Size of a char: %i byte(s)\n", v);
+printf("Size of an int: %i byte(s)\n", w);
+printf("Size of a long int: %i byte(s)\n", x);
+printf("Size of a long long int: %i byte(s)\n", y);
+printf("Size of a float: %i byte(s)\n", z);
+return (0);
 }
